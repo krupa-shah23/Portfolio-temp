aec313a6a10dd00030feee33089577b9f9e5e627
krupa-shah23 <krupaashah2006@gmail.com>
2026-03-02 22:38:59 +0530

    fix ipv6 gmail issue

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER, // krupashahwork@gmail.com
-      pass: process.env.EMAIL_PASS  // jyzqyhnmzexunmva
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
